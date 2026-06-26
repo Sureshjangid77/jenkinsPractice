27ba3b7d3a04084bb15ca66024cbba2bdf50c075
Suresh Jangid <sureshshsharma9@gmail.com>
2026-06-26 13:09:00 +0530

    Testing Poll SCM

--- jenkins-demo/src/main/java/jenkins_demo/controller/DemoController.java
+++ jenkins-demo/src/main/java/jenkins_demo/controller/DemoController.java
@@ -8,7 +8,7 @@ public class DemoController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello Jenkins....!";
+        return "Hello Jenkins.... auto updated !";
     }
 
 }

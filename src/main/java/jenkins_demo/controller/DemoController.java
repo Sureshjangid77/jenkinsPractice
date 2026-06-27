9e0860eebdf9b44c4efa84836d9e33051eb80c86
Suresh Jangid <sureshshsharma9@gmail.com>
2026-06-27 16:01:34 +0530

    commit

--- src/main/java/jenkins_demo/controller/DemoController.java
+++ src/main/java/jenkins_demo/controller/DemoController.java
@@ -8,7 +8,7 @@ public class DemoController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello Jenkins....auto updated successfully webhooks testing!";
+        return "Hello Jenkins....auto updated successfully webhooks testing ,little change!";
     }
 
 }
